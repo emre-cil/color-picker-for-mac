2daa3f5899a9532f6eb02ef7abf90665fba192fa
emre-cil <info.emrecil@gmail.com>
2024-10-10 22:39:42 +0300

    feat: add initial xcode files

---
+++ color-picker-for-mac/color_picker_for_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  color_picker_for_macApp.swift
+//  color-picker-for-mac
+//
+//  Created by Caretta pc on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct color_picker_for_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ color-picker-for-macTests/color_picker_for_macTests.swift
@@ -0,0 +1,17 @@
+//
+//  color_picker_for_macTests.swift
+//  color-picker-for-macTests
+//
+//  Created by Caretta pc on 10.10.2024.
+//
+
+import Testing
+@testable import color_picker_for_mac
+
+struct color_picker_for_macTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
